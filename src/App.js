e11c6e4011ed12c6cacb1cc7036eb84f0c15ba9c
Ryan Kegerreis <Rkegerreis@assemblysoftware.com>
2019-12-23 11:36:25 -0500

    Adding tutorial files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			columnDefs: [
+				{
+					headerName: 'Make',
+					field: 'make',
+					sortable: true,
+					filter: true,
+					checkboxSelection: true
+				},
+				{
+					headerName: 'Model',
+					field: 'model',
+					sortable: true,
+					filter: true
+				},
+				{
+					headerName: 'Price',
+					field: 'price',
+					sortable: true,
+					filter: true
+				}
+			]
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://api.myjson.com/bins/15psn9')
+			.then(result => result.json())
+			.then(rowData => this.setState({ rowData }));
+	}
+
+	render() {
+		this.onClick = e => {
+			const selectedNodes = this.gridApi.getSelectedNodes();
+			const selectedData = selectedNodes.map(node => node.data);
+			const selectedDataStringPresentation = selectedData
+				.map(node => node.make + ' ' + node.model)
+				.join(', ');
+			alert(`Selected nodes: ${selectedDataStringPresentation}`);
+		};
+
+		return (
+			<div
+				className='ag-theme-balham'
+				style={{
+					height: '500px',
+					width: '650px'
+				}}>
+				<button onClick={this.onClick}>Get Selected Rows</button>
+				<AgGridReact
+					onGridReady={params => (this.gridApi = params.api)}
+					columnDefs={this.state.columnDefs}
+					rowData={this.state.rowData}
+					rowSelection='multiple'></AgGridReact>
+			</div>
+		);
+	}
 }
-
-export default App;
